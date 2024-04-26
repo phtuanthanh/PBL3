32a90b43328347c30b16934fc75b38cc71ae546d
phtuanthanh <thanhloveliv2004@gmail.com>
2024-04-26 22:28:23 +0700

    hi

---
+++ DAL/DAL_KhachTro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DAL_KhachTro
+    {
+        public DataTable getLoaiPhong()
+        {
+            string query = "select * from KhachTro";
+            DataTable dtLoaiPhong = DAL_DBHelper.Instance.GetRecords(query);
+            return dtLoaiPhong;
+        }
+
+    }
+}
